c959cf6ec888a5c329e657a6789215cf34687ba0
marina-sam14 <marina.samb14@gmail.com>
2023-04-10 13:58:50 +0300

    GA4

--- pages/_app.js
+++ pages/_app.js
@@ -15,11 +15,17 @@ function MyApp({ Component, pageProps }) {
         dangerouslySetInnerHTML={{
           __html:
             // UA
+            // `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            // new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            // j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            // 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            // })(window,document,'script','dataLayer','GTM-W5DX7H2')`,
+            // GA
             `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-W5DX7H2')`,
+            })(window,document,'script','dataLayer','GTM-WF23TFD')`,
         }}
       ></Script>
     </>
